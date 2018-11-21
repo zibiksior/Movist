85ee89be2c078c30b118c9d628d94d860ee753ee
Zbigniew Frysztak <z.frysztak@futuremind.com>
2018-11-21 21:05:49 +0100

    Updated shortcuts for newest Movist version

--- remote.lua
+++ remote.lua
@@ -1,47 +1,49 @@
 
 local keyboard = libs.keyboard;
 
---@help Lower system volume
+--@help Lower volume
 actions.volume_down = function()
 	keyboard.press("down");
 end
 
---@help Mute system volume
+--@help Mute volume
 actions.volume_mute = function()
-	keyboard.stroke("option", "cmd", "down");
+	keyboard.press("m");
 end
 
---@help Raise system volume
+--@help Raise volume
 actions.volume_up = function()
 	keyboard.press("up");
 end
 
---@help Previous track
+--@help Jump -5s
 actions.previous = function()
-	keyboard.press("left"); 
+	keyboard.press("left");
 end
 
---@help Next track
+--@help Jump +5s
 actions.next = function()
 	keyboard.press("right");
 end
+
 --@help Subtitles slower
 actions.slower = function()
-	keyboard.press("period");
-end
---@help Subtitles faster
-actions.faster = function()
-	keyboard.press("comma");
+	keyboard.stroke("ctrl", "leftshift", "left");
 end
 
---@help Toggle playback state
+--@help Subtitles faster
+actions.faster = function()
+	keyboard.stroke("ctrl", "leftshift", "right");
+end
+
+--@help Play/Pause
 actions.play_pause = function()
 	keyboard.press("space");
 end
 
 --@help Change audio track
 actions.audio = function()
-	keyboard.stroke("ctrl", "option", "cmd", "s");
+	keyboard.stroke("ctrl", "cmd", "a");
 end
 
 --@help Change subtitle track
@@ -51,12 +53,12 @@ end
 
 --@help Delay audio down
 actions.audio_delay_down = function()
-	keyboard.stroke("ctrl","option", "cmd", "left");
+	keyboard.stroke("ctrl","option", "leftshift", "left");
 end
 
 --@help Delay audio up
 actions.audio_delay_up = function()
-	keyboard.stroke("ctrl","option", "cmd", "right");
+	keyboard.stroke("ctrl","option", "leftshift", "right");
 end
 
 --@help Fullscreen
